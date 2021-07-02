5e3379ad736f04664da092b883a03ea0d00b6763
Jonathan Wright <j.wright72127@gmail.com>
2021-07-02 17:05:02 -0600

    Did major revisions and function changes

--- game.js
+++ game.js
@@ -1,126 +1,209 @@
 let cheese = 0;
 
-let clickUpgrades = {
-    pickaxes: {
-        price: 100,
-        quantity: 0,
-        multiplier: 2,
-        upcharge: 25
-    },
-    cutter: {
-        price: 250,
-        quantity: 0,
-        multiplier: 5,
-        upcharge: 50
-    }
-};
-let automaticUpgrades = {
-    rovers: {
-        price: 500,
-        quantity: 0,
-        multiplier: 20,
-        upcharge: 250
-    },
-    rockets: {
-        price: 1000,
-        quantity: 0,
-        multiplier: 50,
-        upcharge: 500
-    }
-};
+// let clickUpgrades = {
+//     pickaxes: {
+//         price: 100,
+//         quantity: 0,
+//         multiplier: 2,
+//         upcharge: 25
+//     },
+//     cutter: {
+//         price: 250,
+//         quantity: 0,
+//         multiplier: 5,
+//         upcharge: 50
+//     }
+// };
 
+// let automaticUpgrades = {
+//     rovers: {
+//         price: 500,
+//         quantity: 0,
+//         multiplier: 20,
+//         upcharge: 250
+//     },
+//     rockets: {
+//         price: 1000,
+//         quantity: 0,
+//         multiplier: 50,
+//         upcharge: 500
+//     }
+// };
+
+const modifiers = {
+    click: {
+        pickaxes: {
+            price: 100,
+            quantity: 0,
+            multiplier: 1,
+            upcharge: 25
+        },
+        cutter: {
+            price: 250,
+            quantity: 0,
+            multiplier: 5,
+            upcharge: 50
+        }
+    },
+    auto: {
+        rovers: {
+            price: 500,
+            quantity: 0,
+            multiplier: 10,
+            upcharge: 250
+        },
+        rockets: {
+            price: 1000,
+            quantity: 0,
+            multiplier: 25,
+            upcharge: 500
+        }
+    }
+}
 
 function mine() {
-    let modifyer = 0
-    for (let key in clickUpgrades) {
-        let item = clickUpgrades[key]
-        console.log('start')
-        if (item.quantity >= 1) {
-            modifyer += (item.quantity * item.multiplier)
-        }
-        console.log('Cheese :', cheese)
-        console.log('Modifyer :', modifyer)
-    }
-    console.log('Modifyer Outside :', modifyer)
-    console.log('Cheese Outside :', cheese)
-    if (modifyer == 0) {
-        cheese = cheese + 1
-    } else if (modifyer == 1) {
-        cheese = cheese + modifyer + 1
-    } else {
-        cheese = cheese + modifyer
-    }
+    cheese += (1 + calcModifiers('click'))
     update()
+    //#region SECTION VERSION 1.0
+    // cheese += 1 + calcModifiers('click')
+    // cheese++
+
+    // let modifyer = 0
+    // for (let key in clickUpgrades) {
+    //     let item = clickUpgrades[key]
+    //     console.log(key)
+    //     if (item.quantity >= 1) {
+    //         modifyer += (item.quantity * item.multiplier)
+    //     }
+    //     console.log('Cheese :', cheese)
+    //     console.log('Modifyer :', modifyer)
+    // }
+    // console.log('Modifyer Outside :', modifyer)
+    // console.log('Cheese Outside :', cheese)
+    // if (modifyer == 0) {
+    //     cheese = cheese + 1
+    // } else if (modifyer == 1) {
+    //     cheese = cheese + modifyer + 1
+    // } else {
+    //     cheese = cheese + modifyer
+    // }
+    //#endregion
 }
 function collectAuto() {
-    let autoModifyer = 0
-    for (let key in automaticUpgrades) {
-        let item = automaticUpgrades[key]
-        if (item.quantity >= 1) {
-            autoModifyer += (item.quantity * item.multiplier)
-        }
-        console.log('Auto Cheese :', cheese)
-        console.log('More Modifyer :', autoModifyer)
+    cheese += calcModifiers('auto')
+    update()
+    //#region SECTION VERSION 1.0
+    // let autoModifyer = 0
+    // for (let key in automaticUpgrades) {
+    //     let item = automaticUpgrades[key]
+    //     if (item.quantity >= 1) {
+    //         autoModifyer += (item.quantity * item.multiplier)
+    //     }
+    //     console.log('Auto Cheese :', cheese)
+    //     console.log('More Modifyer :', autoModifyer)
+    // }
+    // console.log('Auto Modifyer Outside :', autoModifyer)
+    // console.log('More Cheese Outside :', cheese)
+    // if (autoModifyer >= 0) {
+    //     cheese = cheese + autoModifyer
+    // }
+    //#endregion
+}
+function buy(type, name) {
+    let item = modifiers[type][name]
+    if (cheese >= item.price) {
+        cheese = cheese - item.price
+        item.quantity++
+        item.price += item.upcharge
+        update()
     }
-    console.log('Auto Modifyer Outside :', autoModifyer)
-    console.log('More Cheese Outside :', cheese)
-    if (autoModifyer >= 0) {
-        cheese = cheese + autoModifyer
-    }
+    console.log(item)
+    console.log('Item Amount - ', item.quantity)
+    console.log('Items Price - ', item.price)
     update()
 }
-
-function buyPickaxe() {
-    if (cheese >= clickUpgrades.pickaxes.price) {
-        cheese = cheese - clickUpgrades.pickaxes.price
-        clickUpgrades.pickaxes.quantity = clickUpgrades.pickaxes.quantity + 1
-        clickUpgrades.pickaxes.price += clickUpgrades.pickaxes.upcharge
-    }
-    console.log('Pick Multiplier :', clickUpgrades.pickaxes.multiplier)
-    console.log('Pick Quantity :', clickUpgrades.pickaxes.quantity)
-    update()
-}
-function buyCutter() {
-    if (cheese >= clickUpgrades.cutter.price) {
-        cheese = cheese - clickUpgrades.cutter.price
-        clickUpgrades.cutter.quantity = clickUpgrades.cutter.quantity + 1
-        clickUpgrades.pickaxes.price += clickUpgrades.cutter.upcharge
-    }
-    console.log('Cutter Multiplier :', clickUpgrades.cutter.multiplier)
-    console.log('Cutter Quantity :', clickUpgrades.cutter.quantity)
-    update()
-}
-function buyRover() {
-    if (cheese >= automaticUpgrades.rovers.price) {
-        cheese = cheese - automaticUpgrades.rovers.price
-        automaticUpgrades.rovers.quantity = automaticUpgrades.rovers.quantity + 1
-        automaticUpgrades.rovers.price += automaticUpgrades.rovers.upcharge
-    }
-    console.log('Rover Multiplier :', automaticUpgrades.rovers.multiplier)
-    console.log('Rover Quantity :', automaticUpgrades.rovers.quantity)
-    update()
-}
-function buyRocket() {
-    if (cheese >= automaticUpgrades.rockets.price) {
-        cheese = cheese - automaticUpgrades.rockets.price
-        automaticUpgrades.rockets.quantity = automaticUpgrades.rockets.quantity + 1
-        automaticUpgrades.rockets.price += automaticUpgrades.rockets.upcharge
-    }
-    console.log('Rockets Multiplier :', automaticUpgrades.rockets.multiplier)
-    console.log('Rockets Quantity :', automaticUpgrades.rockets.quantity)
-    update()
-}
+//#region SECTION VERSION 1.0
+// function buyPickaxe() {
+//     if (cheese >= modifiers.click.pickaxes.price) {
+//         cheese = cheese - modifiers.click.pickaxes.price
+//         modifiers.click.pickaxes.quantity = modifiers.click.pickaxes.quantity + 1
+//         modifiers.click.pickaxes.price += modifiers.click.pickaxes.upcharge
+//     }
+//     console.log('Pick Multiplier :', modifiers.click.pickaxes.multiplier)
+//     console.log('Pick Quantity :', modifiers.click.pickaxes.quantity)
+//     update()
+// }
+// function buyCutter() {
+//     if (cheese >= modifiers.click.cutter.price) {
+//         cheese = cheese - modifiers.click.cutter.price
+//         modifiers.click.cutter.quantity = modifiers.click.cutter.quantity + 1
+//         modifiers.click.cutter.price += modifiers.click.cutter.upcharge
+//     }
+//     console.log('Cutter Multiplier :', modifiers.click.cutter.multiplier)
+//     console.log('Cutter Quantity :', modifiers.click.cutter.quantity)
+//     update()
+// }
+// function buyRover() {
+//     if (cheese >= modifiers.auto.rovers.price) {
+//         cheese = cheese - modifiers.auto.rovers.price
+//         modifiers.auto.rovers.quantity = modifiers.auto.rovers.quantity + 1
+//         modifiers.auto.rovers.price += modifiers.auto.rovers.upcharge
+//     }
+//     console.log('Rover Multiplier :', modifiers.auto.rovers.multiplier)
+//     console.log('Rover Quantity :', modifiers.auto.rovers.quantity)
+//     update()
+// }
+// function buyRocket() {
+//     if (cheese >= modifiers.auto.rockets.price) {
+//         cheese = cheese - modifiers.auto.rockets.price
+//         modifiers.auto.rockets.quantity = modifiers.auto.rockets.quantity + 1
+//         modifiers.auto.rockets.price += modifiers.auto.rockets.upcharge
+//     }
+//     console.log('Rockets Multiplier :', modifiers.auto.rockets.multiplier)
+//     console.log('Rockets Quantity :', modifiers.auto.rockets.quantity)
+//     update()
+// }
+//#endregion
 
 function startInterval() {
-    setInterval(collectAuto, 5000);
+    setInterval(collectAuto, 3000);
 }
+
+function calcModifiers(type) {
+    let total = 0
+    // iterate over modifiers and return total modified
+    for (let key in modifiers[type]) {
+        let item = modifiers[type][key]
+        total += item.multiplier * item.quantity
+        //#region SECTION Outdated code, starter code
+        // if (item.quantity >= 1 && type == `click`) {
+        //     modifyer += (item.quantity * item.multiplier)
+        // } else if (item.quantity >= 1 && type == `auto`) {
+        //     autoModifyer += (item.quantity * item.multiplier)
+        // }
+        // console.log('An Array - ', item)
+        // console.log('Modifier : ', modifyer)
+        // console.log('Auto Modifier : ', autoModifyer)
+        //#endregion
+        //console.log('Total Modifier : ', total)
+    }
+    //console.log('Outside Total Modifier : ', total)
+    return total
+}
+
 function update() {
+    //Put the id in the dictionary
     document.getElementById("cheeseTotal").innerText = `${cheese}`
-    document.getElementById("pickaxe").innerText = `${clickUpgrades.pickaxes.quantity}`
-    document.getElementById("cutter").innerText = `${clickUpgrades.cutter.quantity}`
-    document.getElementById("pickaxePrice").innerText = `${clickUpgrades.pickaxes.price}`
-    document.getElementById("cutterPrice").innerText = `${clickUpgrades.cutter.price}`
-    document.getElementById("roverPrice").innerText = `${automaticUpgrades.rovers.price}`
-    document.getElementById("rocketPrice").innerText = `${automaticUpgrades.rockets.price}`
+    document.getElementById("pickaxe").innerText = `${modifiers.click.pickaxes.quantity}`
+    document.getElementById("cutter").innerText = `${modifiers.click.cutter.quantity}`
+    document.getElementById("rover").innerText = `${modifiers.auto.rovers.quantity}`
+    document.getElementById("rocket").innerText = `${modifiers.auto.rockets.quantity}`
+    document.getElementById("pickaxePrice").innerText = `${modifiers.click.pickaxes.price}`
+    document.getElementById("cutterPrice").innerText = `${modifiers.click.cutter.price}`
+    document.getElementById("roverPrice").innerText = `${modifiers.auto.rovers.price}`
+    document.getElementById("rocketPrice").innerText = `${modifiers.auto.rockets.price}`
+    //
+    calcModifiers('click')
+    calcModifiers('auto')
 }
 startInterval()
